a359bfcfca3ea8f37f64af009c374d9b7a0e05af
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-09-14 15:50:18 +0530

    Add ignore file

---
+++ parser.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+def parser(file_path):
+    content = open(file_path, 'rb').read()
+    soup = BeautifulSoup(content, 'lxml')
+    table = soup.find('table',{'style':"font-size=larger; background-color:mintcream;"})
+    record = dict()
+    for tr in table.find_all('tr'):
+        tds = [ i.text.replace('.',' ').replace('\xa0','').strip() for i in tr.find_all('td') ]
+        if len(tds) == 2:
+            record[tds[0]] = tds[1]
+
+    for table in soup.find_all('table',{'style':"font-size=larger; "}):
+        for tr in table.find_all('tr'):
+            for td in tr.find_all('td'):
+                tds = [ i.text.replace('.',' ').strip() for i in td.find_all('font') ]
+                tds = [ i.replace(':','').replace('\xa0','').strip() for i in tds ]
+                if len(tds) == 2:
+                    record[tds[0]] = tds[1]
+    return record
+
+file_path = '/Users/kamlesh/WorkSpace/ip/public/550582de1f354bdeb43201e0561b5080.html'
+aa = parser(file_path)
+print(aa)
